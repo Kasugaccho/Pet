22b71f54e63665b22f060216169f207af45b9436
Kasugaccho <30593725+Kasugaccho@users.noreply.github.com>
2018-06-29 02:52:22 +0900

    餌の実装

--- pet/Source.cpp
+++ pet/Source.cpp
@@ -1,5 +1,6 @@
 #define ASLIB_INCLUDE_DL
 #include <AsLib.hpp>
+#include <forward_list>
 
 struct Pet {
 	AnimeUI ui;
@@ -15,17 +16,31 @@ int32_t asMain()
 	MainControl as(u8"a", Pos2(540, 960));
 
 	//アニメーションファイル
-	const AnimeMainData petA(5, AsLoadTex(u8"Picture/Pet/pet.png", 5));
+	const AnimeMainData petA(5, asLoadTex(u8"Picture/Pet/pet.png", 5));
 	//画像ファイル
-	const TextureMainData bg(AsLoadTex(u8"Picture/BG/bg.png"));
+	const AnimeMainData foodA(1, asLoadTex(u8"Picture/Pet/food.png", 1));
+	const TextureMainData bg(asLoadTex(u8"Picture/BG/bg.png"));
 
 	//アニメーション管理ファイル
 	Pet pet(&petA, 255, PosA4R(100.0f, 100.0f, 50.0f, 50.0f), 10);
+	std::vector<AnimeUI> food;
+	size_t r = size_t(asRand8(10));
+	food.resize(r);
+	for (AnimeUI& i : food) {
+		i.setUI(&foodA, 255, PosA4R(float(asRand32(asWindowSize().x)), 0.0f, 20.0f, 20.0f));
+	}
 
 	while (asLoop())
 	{
 		bg.draw();
-		pet.ui.fpsUpdate().addSizeF(Pos2R(0.1f, 0.1f)).drawF();
+		pet.ui.fpsUpdate().addSizeF(Pos2R(0.1f, 0.01f)).drawF();
+		for (size_t i = 0; i < r; ++i) {
+			if (food[i].addPosF(0.0f, 0.9f).drawF().isOutWindowF()) {
+				food.erase(food.begin() + i);
+				--r;
+				--i;
+			}
+		}
 	}
 
 	return 0;
